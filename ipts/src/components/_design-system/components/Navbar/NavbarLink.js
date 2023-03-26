0820d8db355dfcaecb46fbff8d66ddec945e4a33
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2023-03-26 12:57:06 -0400

    navlink linked to typography component

--- ipts/src/components/_design-system/components/Navbar/NavbarLink.js
+++ ipts/src/components/_design-system/components/Navbar/NavbarLink.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Typography from '../Typography';
 
 const NavbarLink = (props) => {
 	return (
 		<a href={props.href} className='flex gap-1 align-middle items-center pt-1'>
-			<h2 className='subtitle-1'>{props.children}</h2>
+			<Typography variant='subtitle-1'>{props.children}</Typography>
 			{props.hasIcon === 'true' && (
 				<i class='fa-solid fa-chevron-down fa-xs pb-1'></i>
 			)}
