810b76fe5da055cf6e3acde6e6eef71c1362a79c
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2023-03-26 12:57:50 -0400

    added info and button to infopage

--- ipts/src/components/Info/InfoLanding.js
+++ ipts/src/components/Info/InfoLanding.js
@@ -8,6 +8,8 @@ import Card from '../_design-system/components/Card/Card';
 import Typography from '../_design-system/components/Typography';
 import CardMedia from '../_design-system/components/Card/CardMedia';
 import CardContent from '../_design-system/components/Card/CardContent';
+import Info from '../_design-system/components/Info';
+import Button from '../_design-system/components/Button';
 
 const InfoLanding = () => {
 	return (
@@ -81,15 +83,34 @@ const InfoLanding = () => {
 			</div>
 			<About />
 			<p>Planets we visit</p>
+			<Info
+				variant='img-left'
+				image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80'>
+				<div>
+					<Typography variant='heading-3'>Title</Typography>
+					<Typography variant='body'>Description</Typography>
+				</div>
+				<div>
+					<Typography variant='heading-3'>Title</Typography>
+					<Typography variant='body'>Description</Typography>
+				</div>
+			</Info>
+			<Info
+				variant='img-right'
+				image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80'>
+				<div>
+					<Typography variant='heading-3'>Title</Typography>
+					<Typography variant='body'>Description</Typography>
+				</div>
+				<div>
+					<Typography variant='heading-3'>Title</Typography>
+					<Typography variant='body'>Description</Typography>
+					<Button>Hello World</Button>
+				</div>
+			</Info>
+
 			<News />
 			<div className='p-10 flex gap-4 justify-center'>
-				<Card>
-					<CardMedia image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80' />
-					<CardContent>
-						<Typography variant='subtitle-1'>Title</Typography>
-						<Typography variant='body'>Description</Typography>
-					</CardContent>
-				</Card>
 				<Card>
 					<CardMedia image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80' />
 					<CardContent>
