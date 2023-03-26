ad11e2a3c4a4c9ef34a4a8d91b992c570606154c
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2023-03-26 12:56:40 -0400

    Typography modified

--- ipts/src/components/_design-system/components/Typography.js
+++ ipts/src/components/_design-system/components/Typography.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Typography = (props) => {
+	let mergedClass;
+
 	function getClass(type) {
 		var textClass = {
 			'heading-1': 'heading-1',
@@ -12,8 +14,25 @@ const Typography = (props) => {
 		};
 		return textClass[type];
 	}
-	const textClass = getClass(props.variant);
-	return <div class={textClass}>{props.children}</div>;
+
+	if (props.class) {
+		mergedClass = props.className + getClass(props.variant);
+	} else {
+		mergedClass = getClass(props.variant);
+	}
+
+	function getTypography(variant) {
+		var semantic = {
+			'heading-1': <h1 className={mergedClass}>{props.children}</h1>,
+			'heading-2': <h2 className={mergedClass}>{props.children}</h2>,
+			'heading-3': <h3 className={mergedClass}>{props.children}</h3>,
+			'subtitle-1': <p className={mergedClass}>{props.children}</p>,
+			body: <p className={mergedClass}>{props.children}</p>,
+			caption: <caption className={mergedClass}>{props.children}</caption>
+		};
+		return semantic[variant];
+	}
+	return getTypography(props.variant);
 };
 
 export default Typography;
