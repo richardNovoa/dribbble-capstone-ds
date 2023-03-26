5fe0c0f75c33649fd2ad32b9243e620bbe5952f0
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2023-03-26 12:57:24 -0400

    Create Info.js

---
+++ ipts/src/components/_design-system/components/Info.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Info = (props) => {
+	if (props.variant === 'img-left') {
+		return (
+			<div className='flex p-10 w-full justify-center'>
+				<img src={props.image} alt='demo' className='w-4/5' />
+				<div className='flex flex-col justify-between p-10 w-80'>
+					{props.children}
+				</div>
+			</div>
+		);
+	}
+	if (props.variant === 'img-right') {
+		return (
+			<div className='flex p-10 w-full justify-center'>
+				<div className='flex flex-col justify-between p-10 w-80'>
+					{props.children}
+				</div>
+				<img src={props.image} alt='demo' className='w-4/5' />
+			</div>
+		);
+	}
+};
+
+export default Info;
