1493a76e2422b034eb1c6c06e76523077c29a51a
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2023-03-29 00:23:14 -0400

    Update Info.js

--- design-system/src/lib/components/Info.js
+++ design-system/src/lib/components/Info.js
@@ -4,7 +4,11 @@ const Info = (props) => {
 	if (props.variant === 'img-left') {
 		return (
 			<div className='flex p-10 w-full justify-center'>
-				<img src={props.image} alt='demo' className='w-4/5' />
+				<img
+					src={props.image}
+					alt='demo'
+					className='w-4/5 border-2 border-capstonered-300'
+				/>
 				<div className='flex flex-col justify-between p-10 w-80'>
 					{props.children}
 				</div>
@@ -17,7 +21,11 @@ const Info = (props) => {
 				<div className='flex flex-col justify-between p-10 w-80'>
 					{props.children}
 				</div>
-				<img src={props.image} alt='demo' className='w-4/5' />
+				<img
+					src={props.image}
+					alt='demo'
+					className='w-4/5 border-2 border-capstonered-300'
+				/>
 			</div>
 		);
 	}
