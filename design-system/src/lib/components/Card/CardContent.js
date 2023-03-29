84aa229747f328651ea8fc44d6b1aa10ce28b783
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2023-03-29 00:18:08 -0400

    Update CardContent.js

--- design-system/src/lib/components/Card/CardContent.js
+++ design-system/src/lib/components/Card/CardContent.js
@@ -4,7 +4,11 @@ const CardContent = (props) => {
 	if (props.variant === 'sm') {
 		return <div className='py-2'>{props.children}</div>;
 	} else {
-		return <div className='p-4 bg-white'>{props.children}</div>;
+		return (
+			<div className='p-4 bg-capstoneblue-1000 text-white'>
+				{props.children}
+			</div>
+		);
 	}
 };
 
