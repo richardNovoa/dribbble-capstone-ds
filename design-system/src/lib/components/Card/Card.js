cb772f0da6e69e9f9cd22ad42d4ffad5bda4997f
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2023-03-29 00:18:05 -0400

    Update Card.js

--- design-system/src/lib/components/Card/Card.js
+++ design-system/src/lib/components/Card/Card.js
@@ -4,11 +4,7 @@ const Card = (props) => {
 	if (props.variant === 'sm') {
 		return <div className='rounded overflow-hidden'>{props.children}</div>;
 	} else {
-		return (
-			<div class='rounded overflow-hidden bg-white shadow'>
-				{props.children}
-			</div>
-		);
+		return <div class='rounded overflow-hidden w-64'>{props.children}</div>;
 	}
 };
 
