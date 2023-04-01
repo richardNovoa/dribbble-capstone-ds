584dbd123f78863b3a24039a8725b266f6b27470
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2023-04-01 16:50:53 -0400

    Revert "Update App.js"
    This reverts commit f7d72ac6e61672e8b492c9a44829603255c2c03e.

--- design-system/src/App.js
+++ design-system/src/App.js
@@ -12,16 +12,127 @@ import Banner from './lib/components/Banner';
 function App() {
 	return (
 		<div>
-			<h1>Styleguide Title</h1>
-			<p>Navbar</p>
-			<Navbar>
-				<NavbarLink>How are you</NavbarLink>
+			<Navbar variant='primary'>
+				<NavbarLink href='#'>Travel</NavbarLink>
+				<NavbarLink href='#'>Rail</NavbarLink>
+				<NavbarLink href='#' hasIcon='true'>
+					EN
+				</NavbarLink>
 			</Navbar>
-			<p>Display Image</p>
-			<p>Card</p>
-			<p>Typography</p>
-			<p>Decorative</p>
-			<p>Footer</p>
+			<Navbar variant='secondary'>
+				<NavbarLink href='#'>Travel</NavbarLink>
+				<NavbarLink href='#'>Rail</NavbarLink>
+				<NavbarLink href='#' hasIcon='true'>
+					EN
+				</NavbarLink>
+			</Navbar>
+			<div className='p-10 flex gap-4 justify-center'>
+				<Card variant='sm'>
+					<CardMedia
+						variant='sm'
+						image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80'
+					/>
+					<CardContent variant='sm'>
+						<Typography variant='subtitle-1'>Title</Typography>
+						<Typography variant='body'>Description</Typography>
+					</CardContent>
+				</Card>
+				<Card variant='sm'>
+					<CardMedia
+						variant='sm'
+						image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80'
+					/>
+					<CardContent variant='sm'>
+						<Typography variant='subtitle-1'>Title</Typography>
+						<Typography variant='body'>Description</Typography>
+					</CardContent>
+				</Card>
+				<Card variant='sm'>
+					<CardMedia
+						variant='sm'
+						image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80'
+					/>
+					<CardContent variant='sm'>
+						<Typography variant='subtitle-1'>Title</Typography>
+						<Typography variant='body'>Description</Typography>
+					</CardContent>
+				</Card>
+				<Card variant='sm'>
+					<CardMedia
+						variant='sm'
+						image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80'
+					/>
+					<CardContent variant='sm'>
+						<Typography variant='subtitle-1'>Title</Typography>
+						<Typography variant='body'>Description</Typography>
+					</CardContent>
+				</Card>
+				<Card variant='sm'>
+					<CardMedia
+						variant='sm'
+						image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80'
+					/>
+					<CardContent variant='sm'>
+						<Typography variant='subtitle-1'>Title</Typography>
+						<Typography variant='body'>Description</Typography>
+					</CardContent>
+				</Card>
+			</div>
+			<p>Planets we visit</p>
+
+			<DisplayImage
+				variant='img-left'
+				image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80'
+			/>
+			<DisplayImage
+				variant='img-right'
+				image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80'
+			/>
+
+			<div className='p-10 flex gap-4 justify-center'>
+				<Card>
+					<CardMedia image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80' />
+					<CardContent>
+						<Typography variant='subtitle-1'>Title</Typography>
+						<Typography variant='body'>Description</Typography>
+					</CardContent>
+				</Card>
+				<Card>
+					<CardMedia image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80' />
+					<CardContent>
+						<Typography variant='subtitle-1'>Title</Typography>
+						<Typography variant='body'>Description</Typography>
+					</CardContent>
+				</Card>
+				<Card>
+					<CardMedia image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80' />
+					<CardContent>
+						<Typography variant='subtitle-1'>Title</Typography>
+						<Typography variant='body'>Description</Typography>
+					</CardContent>
+				</Card>
+				<Card>
+					<CardMedia image='https://images.unsplash.com/photo-1586999082731-574a06ec7e25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80' />
+					<CardContent>
+						<Typography variant='subtitle-1'>Title</Typography>
+						<Typography variant='body'>Description</Typography>
+					</CardContent>
+				</Card>
+			</div>
+			<p>call to action</p>
+			<Banner>
+				<Typography variant='heading-3'>
+					Join the interplanetary travel syndicate
+				</Typography>
+				<Typography variant='body'>
+					For a limited time, we're offering an exclusive deal to our new
+					customers. <br />
+					Be warned, this offer won't last forever.
+				</Typography>
+				<Button>CONSCRIPT NOW</Button>
+			</Banner>
+
+			<Footer />
 		</div>
 	);
 }
