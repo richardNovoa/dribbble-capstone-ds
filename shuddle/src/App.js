adef60e713c483bad6e1dc3914b64150857acc07
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2023-04-01 21:23:24 -0400

    cleanup

--- shuddle/src/App.js
+++ shuddle/src/App.js
@@ -1,27 +1,17 @@
-import Navbar from 'capstone-ds-rn/dist/components/Navbar/Navbar';
-import NavbarLink from 'capstone-ds-rn/dist/components/Navbar/NavbarLink';
-import Typography from 'capstone-ds-rn/dist/components/Typography';
-import Card from 'capstone-ds-rn/dist/components/Card/Card';
-import CardMedia from 'capstone-ds-rn/dist/components/Card/CardMedia';
+import { Navbar, NavbarLink } from 'capstone-ds-rn/dist/components/Navbar';
 
 function App() {
 	return (
 		<div>
-			<div className='py-10'>
-				<Navbar variant='primary'>
-					<NavbarLink>Hello world</NavbarLink>
-					<NavbarLink>Hello world</NavbarLink>
-					<NavbarLink>Hello world</NavbarLink>
-				</Navbar>
-				<Navbar variant='secondary'>
-					<NavbarLink>Hello world</NavbarLink>
-					<NavbarLink>Hello world</NavbarLink>
-					<NavbarLink>Hello world</NavbarLink>
-				</Navbar>
-			</div>
-			<div className='py-10'>
-				<Card></Card>
-			</div>
+		<Navbar hasSecondary brand='world'>
+				<NavbarLink>Hello</NavbarLink>
+			</Navbar>
+			<Navbar hasSecondary brand='visit'>
+				<NavbarLink>Hello</NavbarLink>
+			</Navbar>
+			<Navbar hasSecondary brand='ride'>
+				<NavbarLink>Hello</NavbarLink>
+			</Navbar>
 		</div>
 	);
 }
